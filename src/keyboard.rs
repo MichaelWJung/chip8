59477979b2d0df5869acbf004596f55fc28d3e19
Michael Jung <MichaelWJung@users.noreply.github.com>
2017-09-03 17:57:38 +0200

    Fix remaining keyboard issue

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -4,6 +4,7 @@ use sdl2::keyboard::Keycode;
 
 pub struct Keyboard<'a> {
     key_statuses: [bool; 16],
+    key_press_pending: bool,
     event_pump: &'a mut EventPump
 }
 
@@ -27,21 +28,25 @@ const KEY_F: Keycode = Keycode::J;
 
 impl<'a> Keyboard<'a> {
     pub fn new(event_pump: &'a mut EventPump) -> Keyboard<'a> {
-        Keyboard { key_statuses: [false; 16], event_pump }
+        Keyboard { key_statuses: [false; 16], key_press_pending: false, event_pump }
     }
 
-    pub fn is_pressed(&self, key: u8) -> bool {
+    pub fn is_pressed(&mut self, key: u8) -> bool {
+        self.key_press_pending = false;
         self.key_statuses[key as usize]
     }
 
-    pub fn any_key_pressed(&self) -> Option<u8> {
+    pub fn any_key_pressed(&mut self) -> Option<u8> {
         let mut ret: Option<u8> = None;
-        for i in 0u8..16u8 {
-            if self.key_statuses[i as usize] {
-                ret = Some(i);
-                break;
+        if self.key_press_pending {
+            for i in 0u8..16u8 {
+                if self.key_statuses[i as usize] {
+                    ret = Some(i);
+                    break;
+                }
             }
         }
+        self.key_press_pending = false;
         ret
     }
 
@@ -58,22 +63,22 @@ impl<'a> Keyboard<'a> {
 
     fn update_key_status(&mut self, keycode: Keycode, down: bool) {
         match keycode {
-            KEY_0 => self.key_statuses[0x0] = down,
-            KEY_1 => self.key_statuses[0x1] = down,
-            KEY_2 => self.key_statuses[0x2] = down,
-            KEY_3 => self.key_statuses[0x3] = down,
-            KEY_4 => self.key_statuses[0x4] = down,
-            KEY_5 => self.key_statuses[0x5] = down,
-            KEY_6 => self.key_statuses[0x6] = down,
-            KEY_7 => self.key_statuses[0x7] = down,
-            KEY_8 => self.key_statuses[0x8] = down,
-            KEY_9 => self.key_statuses[0x9] = down,
-            KEY_A => self.key_statuses[0xA] = down,
-            KEY_B => self.key_statuses[0xB] = down,
-            KEY_C => self.key_statuses[0xC] = down,
-            KEY_D => self.key_statuses[0xD] = down,
-            KEY_E => self.key_statuses[0xE] = down,
-            KEY_F => self.key_statuses[0xF] = down,
+            KEY_0 => { self.key_statuses[0x0] = down; self.key_press_pending = true; },
+            KEY_1 => { self.key_statuses[0x1] = down; self.key_press_pending = true; },
+            KEY_2 => { self.key_statuses[0x2] = down; self.key_press_pending = true; },
+            KEY_3 => { self.key_statuses[0x3] = down; self.key_press_pending = true; },
+            KEY_4 => { self.key_statuses[0x4] = down; self.key_press_pending = true; },
+            KEY_5 => { self.key_statuses[0x5] = down; self.key_press_pending = true; },
+            KEY_6 => { self.key_statuses[0x6] = down; self.key_press_pending = true; },
+            KEY_7 => { self.key_statuses[0x7] = down; self.key_press_pending = true; },
+            KEY_8 => { self.key_statuses[0x8] = down; self.key_press_pending = true; },
+            KEY_9 => { self.key_statuses[0x9] = down; self.key_press_pending = true; },
+            KEY_A => { self.key_statuses[0xA] = down; self.key_press_pending = true; },
+            KEY_B => { self.key_statuses[0xB] = down; self.key_press_pending = true; },
+            KEY_C => { self.key_statuses[0xC] = down; self.key_press_pending = true; },
+            KEY_D => { self.key_statuses[0xD] = down; self.key_press_pending = true; },
+            KEY_E => { self.key_statuses[0xE] = down; self.key_press_pending = true; },
+            KEY_F => { self.key_statuses[0xF] = down; self.key_press_pending = true; },
             _ => ()
         }
     }
